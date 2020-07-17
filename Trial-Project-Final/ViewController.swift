cd9524af6d29cea2a1cecef8b461f845ac47c10e
hung977 <hidden@gmail.com>
2020-07-17 11:43:56 +0700

    Initial Commit

---
+++ Trial-Project-Final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trial-Project-Final
+//
+//  Created by TPS on 7/17/20.
+//  Copyright © 2020 TPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
